b25b602959cd8f5438691e5fb14ed84994808c73
Matthaios Stavrou <mstavrou@instrasoft-intl.com>
2021-01-29 20:22:25 +0200

    html pages and logic

--- oauth2/api.py
+++ oauth2/api.py
@@ -1,25 +1,38 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from oauth2.otp import otp
 
 
 def index(request):
-    msg = "Hello %s, you're logged in with session token %s <a href=/otp><button>Click Me</button></a>" % (request.session['user']['name'], request.session['token'])
-    return HttpResponse(msg)
+    name = request.session['user']['name']
+    token = request.session['token']
+    #request.session['num_visits'] = num_visits + 1
+    #msg = "Hello %s, you're logged in with session token %s <a href=/otp><button>Click Me</button></a>" % (request.session['user']['name'], request.session['token'])
+    context = {
+        'name': name,
+        'token':token,
+    }
+    return render(request, 'otp.html',context=context)
+    #return HttpResponse(msg)
 
 
 def send_otp(request):
-    from oauth2.otp import otp
     otp_four_digit = otp.generateOTP()
 
     # Maybe encode?
-    plaintext = 'hello world'
-    encrypt = otp.encryption(plaintext, otp_four_digit)
-    encryptedText = encrypt.OTPencryption()
-
+    #plaintext = 'hello world'
+    #encrypt = otp.encryption(plaintext, otp_four_digit)
+    #encryptedText = encrypt.OTPencryption()
+    #import pdb; pdb.set_trace()
     request.session['otp'] = otp_four_digit
-    msg = "Your 4 digit otp is: %s" % otp_four_digit
+    #msg = "Your 4 digit otp is: %s" % otp_four_digit
     send_email_with_otp("gpapadopoulos864@gmail.com", "OTP PIN",
                         "OTP is: %s" % otp_four_digit)
-    return HttpResponse(msg)
+    context = {
+        'otp_input': otp_four_digit,
+    }
+    return render(request, 'authenticate.html',context=context)
+    #return HttpResponse(msg)
 
 
 def send_email_with_otp(email, subject, message):
