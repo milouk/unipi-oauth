39156a1ad2d28055c3659663e553508a8d1f0698
Michael Loukeris <loukerismichalis@gmail.com>
2021-01-31 13:57:54 +0200

    Add QR code

--- oauth2/api.py
+++ oauth2/api.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from oauth2.otp import otp
+import qrcode
 
 
 def index(request):
@@ -42,3 +43,28 @@ def logout(request):
     from oauth2.auth import OAuthMiddleware
     OAuthMiddleware().clear_session(request)
     return redirect('/')
+
+
+def code_qr(request):
+    from PIL import Image
+    import io
+    import base64
+    
+    name = 'Welcome to'
+    qr = qrcode.make('hello')
+    qr.save('myqr.png')
+
+    im = Image.open('/app/myqr.png', mode='r')
+    buffer = io.BytesIO()
+    im.save(buffer, format='PNG')
+    buffer.seek(0)
+
+    data_uri = base64.b64encode(buffer.read()).decode('ascii')
+
+    html = "data:image/png;base64,{0}".format(data_uri)
+
+    context = {
+        'name': name,
+        'image': html,
+    }
+    return render(request, 'qrcode.html', context)
